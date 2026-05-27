0d93af78b551aed407b3a9680f8f5184a0e4b10c
dhanya1232 <phdhanya33@gmail.com>
2026-05-27 18:27:45 +0530

    Add: power-of-two - submission-1

---
+++ Data Structures & Algorithms/power-of-two/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+       if(n<=0){
+        return false;
+       }   
+       long power=1;
+       while(power<n){
+        power=power*2;
+       }
+       return power==n;
+    }
+}

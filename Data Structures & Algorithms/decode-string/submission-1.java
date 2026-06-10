43cb7e73667360b295f95de47c8c5f66794b6f5c
dhanya1232 <phdhanya33@gmail.com>
2026-06-10 18:49:50 +0530

    Add: decode-string - submission-1

---
+++ Data Structures & Algorithms/decode-string/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String decodeString(String s) {
+       Stack<Integer> countStack=new Stack<>();
+       Stack<String> stringStack=new Stack<>();
+        
+       int num=0;
+       String curr="";
+
+       for(char ch:s.toCharArray()){
+           if(Character.isDigit(ch)){
+             num=num*10+(ch-'0');  //used to convert multi-digit char to integer datatype
+           }else if(ch=='['){
+            countStack.push(num);
+            stringStack.push(curr);
+            curr="";
+            num=0;
+           }else if(ch==']'){
+             int repeat=countStack.pop();
+             StringBuilder decodedstring=new StringBuilder(stringStack.pop());
+             for(int i=0;i<repeat;i++){
+                decodedstring.append(curr);
+             }
+             curr=decodedstring.toString();
+           }else{
+            curr=curr+ch;
+           }
+
+       }
+       return curr;
+    }
+}

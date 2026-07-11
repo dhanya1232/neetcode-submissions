c6d3bf28a0f56117474d400b167138a6c8448545
dhanya1232 <phdhanya33@gmail.com>
2026-07-11 20:19:51 +0530

    Add: valid-word-abbreviation - submission-0

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        int i=0;
+        int j=0;
+         
+        while(i<word.length() && j<word.length()){
+            if(Character.isLetter(abbr.charAt(j))){
+                if(word.charAt(i)!=abbr.charAt(j)){
+                    return false;
+                }
+                i++;
+                j++;
+            }else{
+                int num=0;
+                if(abbr.charAt(j)=='0')
+                return false;
+
+                while(j<abbr.length() && Character.isDigit(abbr.charAt(j))){
+                    num=num*10+ (abbr.charAt(j)-'0');
+                    j++;
+                }
+                i=i+num;
+            }
+        }
+        return i==word.length() && j==abbr.length();
+    }
+}
